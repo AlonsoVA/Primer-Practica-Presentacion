bcdb79701315f473bf06c724ecb2956be8a3ef0e
AlonsoVA <128510867+AlonsoVA@users.noreply.github.com>
2024-09-30 00:29:38 -0600

    lista funcionando

--- Listaagreg.js
+++ Listaagreg.js
@@ -1,6 +1,18 @@
-let BotonAgregar = document.querySelector("#BotonAgregar");
-let InputTarea = DocumentTimeline("Texto");
+const BotonAgregar = document.getElementById("Botonan");
+const input_tareas = document.getElementById("inputtar");
+const li_tareas = document.getElementById("Lista-tareas")
 
-BotonAgregar.addEventListener("click", function(){
-    console.log("Se hizo Click")
+BotonAgregar.addEventListener("click", () =>{
+    const valorinput = input_tareas.value;
+
+    const li_activity = document.createElement("li");
+    const text_tareas = document.createTextNode(valorinput);
+    li_activity.appendChild(text_tareas);
+    li_tareas.appendChild(li_activity);
+    const boton_borrar = document.createElement("button");
+    boton_borrar.textContent = "Borrar";
+    li_activity.appendChild(boton_borrar);
+    boton_borrar.addEventListener("click", () =>{
+        li_activity.remove();
+    });
 });
